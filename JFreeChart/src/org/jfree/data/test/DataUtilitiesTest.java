8a14f45eedff88885da30411e11a4799e610d5ba
unknown <antoineb374@gmail.com>
2019-02-12 16:13:47 -0700

    finished calculateRowTotal test

--- JFreeChart/src/org/jfree/data/test/DataUtilitiesTest.java
+++ JFreeChart/src/org/jfree/data/test/DataUtilitiesTest.java
@@ -31,7 +31,7 @@ public class DataUtilitiesTest {
 		//exercise 
 		double result = DataUtilities.calculateColumnTotal(values, -1);
 		// verify
-		assertEquals(result, 0, .000000001d);
+		assertEquals(0, result, .000000001d);
 		// tear-down: NONE in this test method
 	}
 	@Test
@@ -56,7 +56,7 @@ public class DataUtilitiesTest {
 		//exercise 
 		double result = DataUtilities.calculateColumnTotal(values, 0);
 		// verify
-		assertEquals(result, 5, .000000001d);
+		assertEquals(5, result, .000000001d);
 		// tear-down: NONE in this test method
 	}
 	@Test
@@ -81,7 +81,7 @@ public class DataUtilitiesTest {
 		//exercise 
 		double result = DataUtilities.calculateColumnTotal(values, 1);
 		// verify
-		assertEquals(result, 5, .000000001d);
+		assertEquals(5, result, .000000001d);
 		// tear-down: NONE in this test method
 	}
 	@Test
@@ -106,7 +106,7 @@ public class DataUtilitiesTest {
 		//exercise 
 		double result = DataUtilities.calculateColumnTotal(values, 3);
 		// verify
-		assertEquals(result, 5, .000000001d);
+		assertEquals(5, result, .000000001d);
 		// tear-down: NONE in this test method
 	}
 	@Test
@@ -131,7 +131,7 @@ public class DataUtilitiesTest {
 		//exercise 
 		double result = DataUtilities.calculateColumnTotal(values, 4);
 		// verify
-		assertEquals(result, 5, .000000001d);
+		assertEquals(5, result, .000000001d);
 		// tear-down: NONE in this test method
 	}
 	@Test
@@ -146,7 +146,7 @@ public class DataUtilitiesTest {
 		//exercise 
 		double result = DataUtilities.calculateColumnTotal(values, 5);
 		// verify
-		assertEquals(result, 0, .000000001d);
+		assertEquals(0, result, .000000001d);
 		// tear-down: NONE in this test method
 	}
 	@Test
@@ -171,9 +171,171 @@ public class DataUtilitiesTest {
 		//exercise 
 		double result = DataUtilities.calculateColumnTotal(values, 2);
 		// verify
-		assertEquals(result, 5, .000000001d);
+		assertEquals(5, result, .000000001d);
 		// tear-down: NONE in this test method
 	}
 	
 
+	
+	//tests for calculateRowTotal()
+	@Test(expected = InvalidParameterException.class)
+	public void calculateRowTotalForNullTable() {
+		DataUtilities.calculateRowTotal(null, 0);
+		// throws NullPointerException instead 
+	}
+	@Test
+	public void calculateRowTotalBLB() {
+		// setup
+		Mockery mockingContext = new Mockery();
+		final Values2D values = mockingContext.mock(Values2D.class);
+		mockingContext.checking(new Expectations() {{
+			oneOf(values).getColumnCount();
+			will(returnValue(5));
+		}});
+		//exercise 
+		double result = DataUtilities.calculateRowTotal(values, -1);
+		// verify
+		assertEquals(0, result, .000000001d);
+		// tear-down: NONE in this test method
+	}
+	@Test
+	public void calculateRowTotalLB() {
+		// setup
+		Mockery mockingContext = new Mockery();
+		final Values2D values = mockingContext.mock(Values2D.class);
+		mockingContext.checking(new Expectations() {{
+			one(values).getColumnCount();
+			will(returnValue(5));
+			one(values).getValue(0, 0);
+			will(returnValue(1));
+			one(values).getValue(0, 1);
+			will(returnValue(1));
+			one(values).getValue(0, 2);
+			will(returnValue(1));
+			one(values).getValue(0, 3);
+			will(returnValue(1));
+			one(values).getValue(0, 4);
+			will(returnValue(1));
+		}});
+		//exercise 
+		double result = DataUtilities.calculateRowTotal(values, 0);
+		// verify
+		assertEquals(5, result, .000000001d);
+		// tear-down: NONE in this test method
+	}
+	@Test
+	public void calculateRowTotalALB() {
+		// setup
+		Mockery mockingContext = new Mockery();
+		final Values2D values = mockingContext.mock(Values2D.class);
+		mockingContext.checking(new Expectations() {{
+			one(values).getColumnCount();
+			will(returnValue(5));
+			one(values).getValue(1, 0);
+			will(returnValue(1));
+			one(values).getValue(1, 1);
+			will(returnValue(1));
+			one(values).getValue(1, 2);
+			will(returnValue(1));
+			one(values).getValue(1, 3);
+			will(returnValue(1));
+			one(values).getValue(1, 4);
+			will(returnValue(1));
+		}});
+		//exercise 
+		double result = DataUtilities.calculateRowTotal(values, 1);
+		// verify
+		assertEquals(5, result, .000000001d);
+		// tear-down: NONE in this test method
+	}
+	@Test
+	public void calculateRowTotalBUB() {
+		// setup
+		Mockery mockingContext = new Mockery();
+		final Values2D values = mockingContext.mock(Values2D.class);
+		mockingContext.checking(new Expectations() {{
+			one(values).getColumnCount();
+			will(returnValue(5));
+			one(values).getValue(3, 0);
+			will(returnValue(1));
+			one(values).getValue(3, 1);
+			will(returnValue(1));
+			one(values).getValue(3, 2);
+			will(returnValue(1));
+			one(values).getValue(3, 3);
+			will(returnValue(1));
+			one(values).getValue(3, 4);
+			will(returnValue(1));
+		}});
+		//exercise 
+		double result = DataUtilities.calculateRowTotal(values, 3);
+		// verify
+		assertEquals(5, result, .000000001d);
+		// tear-down: NONE in this test method
+	}
+	@Test
+	public void calculateRowTotalUB() {
+		// setup
+		Mockery mockingContext = new Mockery();
+		final Values2D values = mockingContext.mock(Values2D.class);
+		mockingContext.checking(new Expectations() {{
+			one(values).getColumnCount();
+			will(returnValue(5));
+			one(values).getValue(4, 0);
+			will(returnValue(1));
+			one(values).getValue(4, 1);
+			will(returnValue(1));
+			one(values).getValue(4, 2);
+			will(returnValue(1));
+			one(values).getValue(4, 3);
+			will(returnValue(1));
+			one(values).getValue(4, 4);
+			will(returnValue(1));
+		}});
+		//exercise 
+		double result = DataUtilities.calculateRowTotal(values, 4);
+		// verify
+		assertEquals(5, result, .000000001d);
+		// tear-down: NONE in this test method
+	}
+	@Test
+	public void calculateRowTotalAUB() {
+		// setup
+		Mockery mockingContext = new Mockery();
+		final Values2D values = mockingContext.mock(Values2D.class);
+		mockingContext.checking(new Expectations() {{
+			one(values).getColumnCount();
+			will(returnValue(5));
+		}});
+		//exercise 
+		double result = DataUtilities.calculateRowTotal(values, 5);
+		// verify
+		assertEquals(0, result, .000000001d);
+		// tear-down: NONE in this test method
+	}
+	@Test
+	public void calculateRowTotalNOM() {
+		// setup
+		Mockery mockingContext = new Mockery();
+		final Values2D values = mockingContext.mock(Values2D.class);
+		mockingContext.checking(new Expectations() {{
+			one(values).getColumnCount();
+			will(returnValue(5));
+			one(values).getValue(2, 0);
+			will(returnValue(1));
+			one(values).getValue(2, 1);
+			will(returnValue(1));
+			one(values).getValue(2, 2);
+			will(returnValue(1));
+			one(values).getValue(2, 3);
+			will(returnValue(1));
+			one(values).getValue(2, 4);
+			will(returnValue(1));
+		}});
+		//exercise 
+		double result = DataUtilities.calculateRowTotal(values, 2);
+		// verify
+		assertEquals(5, result, .000000001d);
+		// tear-down: NONE in this test method
+	}
 }
