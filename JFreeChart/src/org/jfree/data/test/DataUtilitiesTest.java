c955c5f7cce1a87127898cb1ec2dd82cd3eb8530
unknown <antoineb374@gmail.com>
2019-02-13 12:53:07 -0700

    moved tests in DataUtilitiesTests

--- JFreeChart/src/org/jfree/data/test/DataUtilitiesTest.java
+++ JFreeChart/src/org/jfree/data/test/DataUtilitiesTest.java
@@ -200,6 +200,193 @@ public class DataUtilitiesTest {
 		assertEquals(5, result, .000000001d);
 		// tear-down: NONE in this test method
 	}
+	
+	// tests for calculateRowTotal()
+		@Test(expected = InvalidParameterException.class)
+		public void calculateRowTotalForNullTable() {
+			DataUtilities.calculateRowTotal(null, 0);
+			// throws NullPointerException instead
+		}
+
+		@Test
+		public void calculateRowTotalBLB() {
+			// setup
+			Mockery mockingContext = new Mockery();
+			final Values2D values = mockingContext.mock(Values2D.class);
+			mockingContext.checking(new Expectations() {
+				{
+					oneOf(values).getColumnCount();
+					will(returnValue(5));
+				}
+			});
+			// exercise - test at row=-1
+			double result = DataUtilities.calculateRowTotal(values, -1);
+			// verify
+			//docs say it will return 0 upon invalid input
+			//instead, it does try to get the values at index=-1, causing unexpected invocation jMock.exception 
+			assertEquals(0, result, .000000001d);
+			// tear-down: NONE in this test method
+		}
+
+		@Test
+		public void calculateRowTotalLB() {
+			// setup
+			Mockery mockingContext = new Mockery();
+			final Values2D values = mockingContext.mock(Values2D.class);
+			mockingContext.checking(new Expectations() {
+				{
+					one(values).getColumnCount();
+					will(returnValue(5));
+					one(values).getValue(0, 0);
+					will(returnValue(1));
+					one(values).getValue(0, 1);
+					will(returnValue(1));
+					one(values).getValue(0, 2);
+					will(returnValue(1));
+					one(values).getValue(0, 3);
+					will(returnValue(1));
+					one(values).getValue(0, 4);
+					will(returnValue(1));
+				}
+			});
+			// exercise
+			double result = DataUtilities.calculateRowTotal(values, 0);
+			// verify
+			assertEquals(5, result, .000000001d);
+			// tear-down: NONE in this test method
+		}
+
+		@Test
+		public void calculateRowTotalALB() {
+			// setup
+			Mockery mockingContext = new Mockery();
+			final Values2D values = mockingContext.mock(Values2D.class);
+			mockingContext.checking(new Expectations() {
+				{
+					one(values).getColumnCount();
+					will(returnValue(5));
+					one(values).getValue(1, 0);
+					will(returnValue(1));
+					one(values).getValue(1, 1);
+					will(returnValue(1));
+					one(values).getValue(1, 2);
+					will(returnValue(1));
+					one(values).getValue(1, 3);
+					will(returnValue(1));
+					one(values).getValue(1, 4);
+					will(returnValue(1));
+				}
+			});
+			// exercise
+			double result = DataUtilities.calculateRowTotal(values, 1);
+			// verify
+			assertEquals(5, result, .000000001d);
+			// tear-down: NONE in this test method
+		}
+
+		@Test
+		public void calculateRowTotalBUB() {
+			// setup
+			Mockery mockingContext = new Mockery();
+			final Values2D values = mockingContext.mock(Values2D.class);
+			mockingContext.checking(new Expectations() {
+				{
+					one(values).getColumnCount();
+					will(returnValue(5));
+					one(values).getValue(3, 0);
+					will(returnValue(1));
+					one(values).getValue(3, 1);
+					will(returnValue(1));
+					one(values).getValue(3, 2);
+					will(returnValue(1));
+					one(values).getValue(3, 3);
+					will(returnValue(1));
+					one(values).getValue(3, 4);
+					will(returnValue(1));
+				}
+			});
+			// exercise
+			double result = DataUtilities.calculateRowTotal(values, 3);
+			// verify
+			assertEquals(5, result, .000000001d);
+			// tear-down: NONE in this test method
+		}
+
+		@Test
+		public void calculateRowTotalUB() {
+			// setup
+			Mockery mockingContext = new Mockery();
+			final Values2D values = mockingContext.mock(Values2D.class);
+			mockingContext.checking(new Expectations() {
+				{
+					one(values).getColumnCount();
+					will(returnValue(5));
+					one(values).getValue(4, 0);
+					will(returnValue(1));
+					one(values).getValue(4, 1);
+					will(returnValue(1));
+					one(values).getValue(4, 2);
+					will(returnValue(1));
+					one(values).getValue(4, 3);
+					will(returnValue(1));
+					one(values).getValue(4, 4);
+					will(returnValue(1));
+				}
+			});
+			// exercise
+			double result = DataUtilities.calculateRowTotal(values, 4);
+			// verify
+			assertEquals(5, result, .000000001d);
+			// tear-down: NONE in this test method
+		}
+
+		@Test
+		public void calculateRowTotalAUB() {
+			// setup
+			Mockery mockingContext = new Mockery();
+			final Values2D values = mockingContext.mock(Values2D.class);
+			mockingContext.checking(new Expectations() {
+				{
+					one(values).getColumnCount();
+					will(returnValue(5));
+				}
+			});
+			// exercise - test at row > numRows
+			double result = DataUtilities.calculateRowTotal(values, 5);
+			// verify
+			//docs say it will return 0 upon invalid input
+			//instead, it does try to get the values at index=-5, causing unexpected invocation jMock.exception 
+			assertEquals(0, result, .000000001d);
+			// tear-down: NONE in this test method
+		}
+
+		@Test
+		public void calculateRowTotalNOM() {
+			// setup
+			Mockery mockingContext = new Mockery();
+			final Values2D values = mockingContext.mock(Values2D.class);
+			mockingContext.checking(new Expectations() {
+				{
+					one(values).getColumnCount();
+					will(returnValue(5));
+					one(values).getValue(2, 0);
+					will(returnValue(1));
+					one(values).getValue(2, 1);
+					will(returnValue(1));
+					one(values).getValue(2, 2);
+					will(returnValue(1));
+					one(values).getValue(2, 3);
+					will(returnValue(1));
+					one(values).getValue(2, 4);
+					will(returnValue(1));
+				}
+			});
+			// exercise
+			double result = DataUtilities.calculateRowTotal(values, 2);
+			// verify
+			assertEquals(5, result, .000000001d);
+			// tear-down: NONE in this test method
+		}
 
 	// tests for createNumberArray
 	@Test(expected = InvalidParameterException.class)
@@ -261,192 +448,6 @@ public class DataUtilitiesTest {
 		assertArrayEquals(expected, r);
 	}
 
-	// tests for calculateRowTotal()
-	@Test(expected = InvalidParameterException.class)
-	public void calculateRowTotalForNullTable() {
-		DataUtilities.calculateRowTotal(null, 0);
-		// throws NullPointerException instead
-	}
-
-	@Test
-	public void calculateRowTotalBLB() {
-		// setup
-		Mockery mockingContext = new Mockery();
-		final Values2D values = mockingContext.mock(Values2D.class);
-		mockingContext.checking(new Expectations() {
-			{
-				oneOf(values).getColumnCount();
-				will(returnValue(5));
-			}
-		});
-		// exercise - test at row=-1
-		double result = DataUtilities.calculateRowTotal(values, -1);
-		// verify
-		//docs say it will return 0 upon invalid input
-		//instead, it does try to get the values at index=-1, causing unexpected invocation jMock.exception 
-		assertEquals(0, result, .000000001d);
-		// tear-down: NONE in this test method
-	}
-
-	@Test
-	public void calculateRowTotalLB() {
-		// setup
-		Mockery mockingContext = new Mockery();
-		final Values2D values = mockingContext.mock(Values2D.class);
-		mockingContext.checking(new Expectations() {
-			{
-				one(values).getColumnCount();
-				will(returnValue(5));
-				one(values).getValue(0, 0);
-				will(returnValue(1));
-				one(values).getValue(0, 1);
-				will(returnValue(1));
-				one(values).getValue(0, 2);
-				will(returnValue(1));
-				one(values).getValue(0, 3);
-				will(returnValue(1));
-				one(values).getValue(0, 4);
-				will(returnValue(1));
-			}
-		});
-		// exercise
-		double result = DataUtilities.calculateRowTotal(values, 0);
-		// verify
-		assertEquals(5, result, .000000001d);
-		// tear-down: NONE in this test method
-	}
-
-	@Test
-	public void calculateRowTotalALB() {
-		// setup
-		Mockery mockingContext = new Mockery();
-		final Values2D values = mockingContext.mock(Values2D.class);
-		mockingContext.checking(new Expectations() {
-			{
-				one(values).getColumnCount();
-				will(returnValue(5));
-				one(values).getValue(1, 0);
-				will(returnValue(1));
-				one(values).getValue(1, 1);
-				will(returnValue(1));
-				one(values).getValue(1, 2);
-				will(returnValue(1));
-				one(values).getValue(1, 3);
-				will(returnValue(1));
-				one(values).getValue(1, 4);
-				will(returnValue(1));
-			}
-		});
-		// exercise
-		double result = DataUtilities.calculateRowTotal(values, 1);
-		// verify
-		assertEquals(5, result, .000000001d);
-		// tear-down: NONE in this test method
-	}
-
-	@Test
-	public void calculateRowTotalBUB() {
-		// setup
-		Mockery mockingContext = new Mockery();
-		final Values2D values = mockingContext.mock(Values2D.class);
-		mockingContext.checking(new Expectations() {
-			{
-				one(values).getColumnCount();
-				will(returnValue(5));
-				one(values).getValue(3, 0);
-				will(returnValue(1));
-				one(values).getValue(3, 1);
-				will(returnValue(1));
-				one(values).getValue(3, 2);
-				will(returnValue(1));
-				one(values).getValue(3, 3);
-				will(returnValue(1));
-				one(values).getValue(3, 4);
-				will(returnValue(1));
-			}
-		});
-		// exercise
-		double result = DataUtilities.calculateRowTotal(values, 3);
-		// verify
-		assertEquals(5, result, .000000001d);
-		// tear-down: NONE in this test method
-	}
-
-	@Test
-	public void calculateRowTotalUB() {
-		// setup
-		Mockery mockingContext = new Mockery();
-		final Values2D values = mockingContext.mock(Values2D.class);
-		mockingContext.checking(new Expectations() {
-			{
-				one(values).getColumnCount();
-				will(returnValue(5));
-				one(values).getValue(4, 0);
-				will(returnValue(1));
-				one(values).getValue(4, 1);
-				will(returnValue(1));
-				one(values).getValue(4, 2);
-				will(returnValue(1));
-				one(values).getValue(4, 3);
-				will(returnValue(1));
-				one(values).getValue(4, 4);
-				will(returnValue(1));
-			}
-		});
-		// exercise
-		double result = DataUtilities.calculateRowTotal(values, 4);
-		// verify
-		assertEquals(5, result, .000000001d);
-		// tear-down: NONE in this test method
-	}
-
-	@Test
-	public void calculateRowTotalAUB() {
-		// setup
-		Mockery mockingContext = new Mockery();
-		final Values2D values = mockingContext.mock(Values2D.class);
-		mockingContext.checking(new Expectations() {
-			{
-				one(values).getColumnCount();
-				will(returnValue(5));
-			}
-		});
-		// exercise - test at row > numRows
-		double result = DataUtilities.calculateRowTotal(values, 5);
-		// verify
-		//docs say it will return 0 upon invalid input
-		//instead, it does try to get the values at index=-5, causing unexpected invocation jMock.exception 
-		assertEquals(0, result, .000000001d);
-		// tear-down: NONE in this test method
-	}
-
-	@Test
-	public void calculateRowTotalNOM() {
-		// setup
-		Mockery mockingContext = new Mockery();
-		final Values2D values = mockingContext.mock(Values2D.class);
-		mockingContext.checking(new Expectations() {
-			{
-				one(values).getColumnCount();
-				will(returnValue(5));
-				one(values).getValue(2, 0);
-				will(returnValue(1));
-				one(values).getValue(2, 1);
-				will(returnValue(1));
-				one(values).getValue(2, 2);
-				will(returnValue(1));
-				one(values).getValue(2, 3);
-				will(returnValue(1));
-				one(values).getValue(2, 4);
-				will(returnValue(1));
-			}
-		});
-		// exercise
-		double result = DataUtilities.calculateRowTotal(values, 2);
-		// verify
-		assertEquals(5, result, .000000001d);
-		// tear-down: NONE in this test method
-	}
 
 	//tests for getCumulativePercentage
 	@Test(expected = InvalidParameterException.class)
